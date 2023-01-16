30e0e999f7a206a3a6247e1b259b6101ebb76777
Hoàng Tiến <hoangtien2k3github@gmail.com>
2023-01-16 23:14:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,7 @@
 const CONFIG = {
     titleWeb: "Tien Đẹp Zai",
     introTitle: 'Tiến Rắc Muối',
-    introDesc: `Ba ngàn con sông chỉ uống 1 gáo nước.`,
-    introDesc: `Thiên hạ vạn người chỉ muốn nắm tay em.`,
+    introDesc: `Ba ngàn con sông chỉ uống 1 gáo nước.         Thiên hạ vạn người chỉ muốn nắm tay em.`,
     btnIntro: '^^HiHi^^',
     title: 'Mau Lớn Anh Đợi, hihi 🥰',
     desc: 'Trúc xinh trúc mọc đầu đình. 🥰',
@@ -12,7 +11,7 @@ const CONFIG = {
     question: 'Gọi em là cái kem. Nóng thế này anh chỉ muốn mờ lemmm. <3',
     btnReply: 'Gửi tới anh <3',
     reply: 'Gọi anh là mì gói. Nhìn anh em chỉ muốn ăn liền ... <333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
+    mess: 'Mình biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
     messDesc: 'Tối nay 7h, call mình nha, 😘😘yêu cậu nhìuuu.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/hoangtien2k3.vn/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
