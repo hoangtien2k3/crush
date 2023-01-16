9a2ce8edc63794b8e3d4380067e12b4a42260cf2
Hoàng Tiến <hoangtien2k3github@gmail.com>
2023-01-16 22:50:05 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,20 +1,17 @@
 const CONFIG = {
-    titleWeb: "Tien Đẹp Trai",
-    introTitle: 'Yêu Ai Đây ?',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    titleWeb: "Tien Đẹp Zai",
+    introTitle: 'Tiến Rắc Muối',
+    introDesc: `Ba ngàn con sông chỉ uống 1 gáo nước. Thiên hạ vạn người chỉ muốn nắm tay em.`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    title: 'Trúc xinh trúc mọc đầu đình. 🥰',
+    desc: 'Em xinh em có cởi truồng vẫn xinh. ',
+    btnYes: 'THích cơ 🥰',
+    btnNo: 'Chê nha :)',
+    question: 'Gọi em là cái kem. Nóng thế này anh chỉ muốn mờ lemmm. <3',
+    btnReply: 'Gửi tới anh <3',
+    reply: 'Gọi anh là mì gói. Nhìn anh em chỉ muốn ăn liền ... <333',
     mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, mình qua đón đi chơi nha.',
+    messDesc: 'Tối nay 7h, call mình nha, 😘😘yêu cậu nhìuuu.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/hoangtien2k3.vn/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
