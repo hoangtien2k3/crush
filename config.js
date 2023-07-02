bafe83da7c9d60464cb68dc991981e42b4165bac
Hoàng Anh Tiến <hoangtien2k3qx1@gmail.com>
2023-07-03 02:12:04 +0700

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,15 @@ const CONFIG = {
     introTitle: 'Tiến Rắc Muối',
     introDesc: `Ba ngàn con sông chỉ uống 1 gáo nước. Thiên hạ vạn người chỉ muốn nắm tay em.`,
     btnIntro: '^^HiHi^^',
-    title: 'Cô em váy ngắn tơ hơ nướng. 🥰',
-    desc: 'Mực đỏ dần lên trước lửa cồn.',
+    title: 'Nếu một ngày Windows em báo lỗi. 🥰',
+    desc: 'Anh nguyện làm cái máy mới của em.',
     btnYes: 'Thích cơ 🥰',
     btnNo: 'Chê nha :)',
     question: 'Gọi em là cái kem. Nóng thế này anh chỉ muốn mờ lemmm. <3',
     btnReply: 'Gửi tới anh <3',
     reply: 'Gọi anh là mì gói. Nhìn anh em chỉ muốn ăn liền ... <333',
     mess: 'Mình biết mà 🥰. Yêu cậu nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h, call mình nha, 😘😘yêu cậu nhìuuu.',
+    messDesc: 'Tối nay 7h, call mình nha, 😘😘 yêu cậu nhìuuu.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/hoangtien2k3.vn/' //link mess của các bạn. VD: https://www.facebook.com/messages/t/100014188333536
 }
